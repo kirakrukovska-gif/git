479b7d2ca32973b020df15b2b8ef0597301443a3
kirakrukovska-gif <kirakrukovska@gmail.com>
2025-10-12 21:05:17 +0300

    colorama

---
+++ urok1/colorama1.py
@@ -0,0 +1,11 @@
+import colorama
+
+print(dir(colorama))
+#Найважливіші атрибути:
+#init() - вмикає кольори в консолі
+#Back - задає колір фону(Back.BLUE)
+#Cursor - задає напрям курсору(Cursor.UP(1))
+#Fore - задає колір тексту(Fore.RED)
+#Style - змінює стиль тексту(BRIGHT)
+
+
